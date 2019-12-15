6ee7ed8475aee423befdd6230c8e7c15e0f60d8d
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-16 02:00:44 +0530

    Structure in C

---
+++ 1. Some Essential Concepts/structure.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct rectangle
+{
+  //Creating a structure
+  int length;
+  int breadth;
+}
+
+int main()
+{
+  struct rectangle r;
+  //Declaring structure type variable
+  struct rectangle r={10,5};
+  //Declaring and initialising structure type variable
+  return 0;
+}
