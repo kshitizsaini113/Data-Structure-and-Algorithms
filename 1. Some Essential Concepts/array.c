44b47796bec4b4a869298fbc40baaee38b826435
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-16 01:38:26 +0530

    Array in C

---
+++ 1. Some Essential Concepts/array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+  int a[5];
+  //Declaring an array.
+  int b[5]={1,3,5,7,9};
+  //Declaring as well initialising an array.
+  return 0;
+}
