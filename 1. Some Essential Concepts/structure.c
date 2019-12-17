d2189e291386cd633f208540357a9ef7f318c49c
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-17 10:06:20 +0530

    Stricture in C

--- 1. Some Essential Concepts/structure.c
+++ 1. Some Essential Concepts/structure.c
@@ -5,13 +5,16 @@ struct rectangle
   //Creating a structure
   int length;
   int breadth;
-}
+};
 
 int main()
 {
   struct rectangle r;
   //Declaring structure type variable
-  struct rectangle r={10,5};
+  struct rectangle re={10,5};
   //Declaring and initialising structure type variable
+  re.length=15;
+  re.breadth=10;
+  printf("Area of rectangle is : %d",re.length*re.breadth);
   return 0;
 }
