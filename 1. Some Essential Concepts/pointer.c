d51b2ee694665d2d183fa9325d37bc493a7494b2
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-18 20:06:38 +0530

    Pointers in C

--- 1. Some Essential Concepts/pointer.c
+++ 1. Some Essential Concepts/pointer.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
   int a=3;
   //Variable declared.
   int *p;
+  int *t;
   //Pointer declared
   p=&a;
   //Pointer initialised.
 
-  printf("Pointer is storing address : %d and value %d. ",p,*p); 
+  p=(int *)malloc(5*sizeof(int));
+  //Using pointers to access heap memory as memory allocated dynamically is allocated from heap.
+
+  printf("Pointer is storing address : %d and value %d. ",p,*p);
   return 0;
 }
