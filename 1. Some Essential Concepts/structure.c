4aff2812c54c0179c1398ed55e3baca4edde4947
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-18 19:39:03 +0530

    Structure in C

--- 1. Some Essential Concepts/structure.c
+++ 1. Some Essential Concepts/structure.c
@@ -14,6 +14,7 @@ int main()
   struct rectangle arr[52];
   //Will create array of structures.
   struct rectangle re={10,5};
+  struct rectangle a[2]={{10,5},{5,2}};
   //Declaring and initialising structure type variable
   printf("Area of rectangle is : %d",re.length*re.breadth);
   printf("\n");
