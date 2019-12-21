a96ebb02dc8ac731c50853426db86a873a9d74bf
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-21 12:01:04 +0530

    Function created to initialise structure

--- 1. Some Essential Concepts/structureandfunction.cpp
+++ 1. Some Essential Concepts/structureandfunction.cpp
@@ -8,7 +8,16 @@ struct rectangle
   int bredth;
 };
 
+void initialize(struct rectangle *r, int l, int b)
+{
+  r->length=l;
+  r->bredth=b;
+}
+
 int main()
 {
+  struct rectangle r;
+
+  initialize(&r,10,5);
   return 0;
 }
