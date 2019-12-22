34b709d061f9727177c0a76e6641116a8c02157f
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-22 18:22:03 +0530

    Class code in c++

--- 1. Some Essential Concepts/template.cpp
+++ 1. Some Essential Concepts/template.cpp
@@ -14,7 +14,29 @@ public:
   int sub();
 };
 
+arithmetic::arithmetic(int a, int b)
+{
+  this->a=a;
+  this->b=b;
+}
+
+int arithmetic::add()
+{
+  int c;
+  c=a+b;
+  return c;
+}
+
+int arithmetic::sub()
+{
+  int c;
+  c=a-b;
+  return c;
+}
+
 int main()
 {
+  arithmetic a1(10,5);
+  cout<<a1.add()<<endl<<a1.sub()<<endl;
   return 0;
 }
