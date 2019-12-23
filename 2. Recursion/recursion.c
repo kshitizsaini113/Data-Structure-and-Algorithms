55c96d8c636f2f040b7dfcc285bc2a3b178908b7
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-23 22:41:39 +0530

    Reccursive function

--- 2. Recursion/recursion.c
+++ 2. Recursion/recursion.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
-void fun(int n)
+void fun1(int n)
 {
   if(n>0)
   {
     printf("%d ",n);
-    fun(n-1);
+    fun1(n-1);
+  }
+}
+
+void fun2(int n)
+{
+  if(n>0)
+  {
+    fun2(n-1);
+    printf("%d ",n);
   }
 }
 
@@ -13,6 +22,9 @@ int main()
 {
   int x=3;
 
-  fun(3);
+  fun1(x);
+  printf("\n");
+  fun2(x);
+
   return 0;
 }
