44c8d5aa08b3397b91aca7ce16ace606b78f6a4a
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-24 17:59:58 +0530

    Static variables

--- 2. Recursion/static_variable.c
+++ 2. Recursion/static_variable.c
@@ -8,10 +8,25 @@ int fun(int n)
   }
   return 0;
 }
+//Using auto variable type.
+
+int fun1(int n)
+{
+  static int x=0;
+  if(n>0)
+  {
+    x++;
+    return fun1(n-1)+x;
+  }
+  return 0;
+}
+//Using static variable type.
 
 int main()
 {
   int a=5;
 
   printf("%d",fun(a));
+  printf("\n");
+  printf("%d",fun1(a));
 }
