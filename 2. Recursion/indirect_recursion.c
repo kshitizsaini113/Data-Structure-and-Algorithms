fd26bc5ff4f58df218e2e50fe49608ed0f95abce
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-24 22:27:46 +0530

    Indirect Recursion

--- 2. Recursion/indirect_recursion.c
+++ 2. Recursion/indirect_recursion.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+void funA(int n)
+{
+  if(n>0)
+  {
+    printf("%d ",n);
+    funB(n-1);
+  }
+}
+
+void funB(int n)
+{
+  if(n>0)
+  {
+    printf("%d ",n);
+    funA(n/2);
+  }
+}
+
 int main()
 {
+  funA(20);
   return 0;
 }
