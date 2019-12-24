66665af3777b7fbb98eca10e9dc2dff357f3cdc3
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-24 22:54:49 +0530

    Nested Recursion

--- 2. Recursion/nested_recursion.c
+++ 2. Recursion/nested_recursion.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+int fun(int n)
+{
+  if(n>100)
+  {
+    return n-10;
+  }
+  else
+  {
+    return fun(fun(n+11));
+  }
+}
+
 int main()
 {
+  int r;
+  r=fun(95);
+  printf("%d\n",r);
   return 0;
 }
