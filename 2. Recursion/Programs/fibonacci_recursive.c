88191e458197e45b5af1f7882407d8b93875f6a7
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-25 15:04:00 +0530

    Fibonacci Series

--- 2. Recursion/Programs/fibonacci_recursive.c
+++ 2. Recursion/Programs/fibonacci_recursive.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int f[10];
+
 int fib(int n)
 {
   if(n<=1)
@@ -12,8 +14,36 @@ int fib(int n)
   }
 }
 
+int mfib(int n)
+{
+  if(n<=1)
+  {
+    f[n]=n;
+    return n;
+  }
+  else
+  {
+    if(f[n-2]==-1)
+    {
+      f[n-2]=mfib(n-2);
+    }
+    if(f[n-1]==-1)
+    {
+      f[n-1]=mfib(n-1);
+    }
+    return f[n-1]+f[n-2];
+  }
+}
+
 int main()
-{;
+{
+  int i;
+  for(i=0;i<10;i++)
+  {
+    f[i]=-1;
+  }
   printf("%d",fib(10));
+  printf("\n\n\n");
+  printf("%d",mfib(10));
   return 0;
 }
