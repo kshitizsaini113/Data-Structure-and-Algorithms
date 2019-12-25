15696f319b44c5c58373a67d2f4e8c9820117cc2
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-25 22:17:11 +0530

    Combination using recursion

--- 2. Recursion/Programs/combination.c
+++ 2. Recursion/Programs/combination.c
@@ -4,7 +4,20 @@
 
 #include <stdio.h>
 
+int combination(int n, int r)
+{
+  if(r==0 || n==r)
+  {
+    return 1;
+  }
+  else
+  {
+    return combination(n-1,r-1)+combination(n-1,r);
+  }
+}
+
 int main()
 {
+  printf("%d\n",combination(4,2));
   return 0;
 }
