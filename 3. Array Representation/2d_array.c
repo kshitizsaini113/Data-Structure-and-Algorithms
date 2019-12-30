02c32baf3746dc5e1ae800873c2b51f85118129c
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-30 21:00:58 +0530

    2-D Array

--- 3. Array Representation/2d_array.c
+++ 3. Array Representation/2d_array.c
@@ -5,5 +5,7 @@ int main()
   int a[3][4];
   //2-d array
   //actually memory will be allocated like 1-d array but we are allowed to access elements using two indices.
+  int b[3][4]={{1,2,3,4},{5,6,7,8},{2,4,6,8}};
+  //Declaration and initialisation
   return 0;
 }
