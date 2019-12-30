e210c4c1331242ebc6ca48d642b6c24080ee309b
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-30 23:40:27 +0530

    delete from array

--- 4. Array ADT/delete_from_array.c
+++ 4. Array ADT/delete_from_array.c
@@ -17,6 +17,23 @@ void Display(struct Array arr)
   }
 }
 
+int Delete(struct Array *arr,int index)
+{
+  int x=0;
+  int i;
+  if(index>=0 && index<arr->length)
+  {
+    x=arr->A[index];
+    for(i=index;i<arr->length-1;i++)
+    {
+      arr->A[i]=arr->A[i+1];
+    }
+    arr->length--;
+    return x;
+  }
+  return 0;
+  }
+
 int main()
 {
   return 0;
