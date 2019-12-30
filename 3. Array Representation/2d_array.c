f44f5fe7e85efe2dc6d7acfbfa7efc5d96bffd0d
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-30 21:00:00 +0530

    2-D Array

---
+++ 3. Array Representation/2d_array.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+  int a[3][4];
+  //2-d array
+  //actually memory will be allocated like 1-d array but we are allowed to access elements using two indices.
+  return 0;
+}
