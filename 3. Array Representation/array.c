a73d54750889eb69440da056527d30cd6aeb0303
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-30 13:06:04 +0530

    Array

---
+++ 3. Array Representation/array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int a[5];
+  //Array declared
+  int b[5]={1,2,3,4,5};
+  //Array declared as well as initialised
+  int c[5]={2,4};
+  //Reat elements will be initialised as 0, except the position 0 and 1.
+  return 0;
+}
