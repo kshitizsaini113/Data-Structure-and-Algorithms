8ec67970b0f43378beefc09c0a8c33997e058e88
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-30 13:08:14 +0530

    Array

--- 3. Array Representation/array.c
+++ 3. Array Representation/array.c
@@ -2,11 +2,32 @@
 
 int main()
 {
+  int i;
+
   int a[5];
   //Array declared
   int b[5]={1,2,3,4,5};
   //Array declared as well as initialised
   int c[5]={2,4};
   //Reat elements will be initialised as 0, except the position 0 and 1.
+
+  for(i=0;i<5;i++)
+  {
+      printf("%d   ",a[i] );
+  }
+  printf("\n");
+
+  for(i=0;i<5;i++)
+  {
+      printf("%d   ",b[i] );
+  }
+  printf("\n");
+
+  for(i=0;i<5;i++)
+  {
+      printf("%d   ",c[i] );
+  }
+  printf("\n");
+
   return 0;
 }
