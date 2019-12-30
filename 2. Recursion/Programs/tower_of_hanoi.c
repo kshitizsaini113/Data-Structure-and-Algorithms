2abb3a10d3e39f028d7f6bfda7dd9b0bb743ef80
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-30 11:45:20 +0530

    Tower of Hanoi

--- 2. Recursion/Programs/tower_of_hanoi.c
+++ 2. Recursion/Programs/tower_of_hanoi.c
@@ -1,6 +1,18 @@
-#include <stdioh>
+#include <stdio.h>
+
+void tower_of_hanoi(int n, int A,int B, int C)
+{
+  if (n>0)
+  {
+    tower_of_hanoi(n-1,A,C,B);
+    printf("Move disk from tower %d to tower %d\n",A,C);
+    tower_of_hanoi(n-1,B,A,C);
+  }
+}
 
 int main()
 {
-  return 0; 
+  tower_of_hanoi(3,1,2,3);
+  printf("\n\nTower of Hanoi problem is solved for the given expression.\n");
+  return 0;
 }
