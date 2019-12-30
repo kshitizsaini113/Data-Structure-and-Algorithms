305f7b8c6d4d77589b1ef22dbe3a971729905248
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-30 22:58:58 +0530

    array

---
+++ 4. Array ADT/array.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Array
+{
+  int *a;
+  int size;
+  int length;
+};
+
+void display(struct Array arr)
+{
+  int i;
+  printf("\nElements are\n");
+  for(i=0;i<arr.length;i++)
+  {
+    printf("%d ",arr.a[i]);
+  }
+}
+
+int main()
+{
+  struct Array arr;
+  int n,i;
+
+  printf("Enter size of array : ");
+  scanf("%d",&arr.size);
+  arr.a=(int *)malloc(arr.size*sizeof(int));
+  arr.length=0;
+
+  printf("Enter number of numbers : ");
+  scanf("%d",&n);
+
+  printf("Enter all elements\n");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&arr.a[i]);
+  }
+  arr.length=n;
+
+  display(arr);
+  
+  return 0;
+}
