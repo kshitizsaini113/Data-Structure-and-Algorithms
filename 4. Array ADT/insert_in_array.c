930a5f6fa3dfeefbefaef2e0874bba3435fe4143
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-12-30 23:31:36 +0530

    insert in array

--- 4. Array ADT/insert_in_array.c
+++ 4. Array ADT/insert_in_array.c
@@ -39,5 +39,11 @@ void Insert(struct Array *arr,int index,int x)
 
 int main()
 {
+  struct Array arr1={{2,3,4,5,6},10,5};
+
+  Append(&arr1,10);
+  Insert(&arr1,0,12);
+  Display(arr1);
+  
   return 0;
 }
