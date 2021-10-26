71f3c5989cacfb750356b49af6072173706675fa
Himanshu Parashar <himanshuparashar0101@gmail.com>
2021-10-26 23:28:23 +0530

    Create 019_swaping_two_numbers_without_taking_third_number.cpp

---
+++ 000_BASICS/019_swaping_two_numbers_without_taking_third_number.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using  namespace std;
+int main(){
+	
+	
+	int a,b;
+	cout<<"enter the first number";
+	cin>>a;
+	cout<<"enter the second number";
+	
+	cin>>b;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	cout<<" after the swaping the number the result will be "<< a <<" and "<< b;
+	return 0;
+}
